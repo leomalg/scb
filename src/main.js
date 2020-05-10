7d168e4f3ccd88beef36ded502e01732f1abf6a1
lmalg <leomalgrange@gmail.com>
2020-05-10 22:26:31 +0200

    feature: init project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import FlagIcon from 'vue-flag-icon'
+import './assets/styles.scss'
+
+Vue.use(Buefy)
+Vue.use(FlagIcon)
 
 Vue.config.productionTip = false
 
