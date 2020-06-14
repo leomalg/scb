ef5169b5a9528a544bfbc661ee3e59f5d8dd9a05
leomalg <leomalgrange@gmail.com>
2020-06-14 14:06:35 +0200

    feature: wip

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img src="./../assets/scb.png" alt="Super Ciné Battle" />
+        </b-navbar-item>
+      </template>
+
+      <template slot="start">
+        <b-navbar-dropdown label="Les listes ultimes">
+          <b-navbar-item tag="router-link" :to="{ path: '60' }">Années 60</b-navbar-item>
+          <b-navbar-item tag="router-link" :to="{ path: '70' }">Années 70</b-navbar-item>
+          <b-navbar-item tag="router-link" :to="{ path: '80' }">Années 80</b-navbar-item>
+          <b-navbar-item tag="router-link" :to="{ path: '90' }">Années 90</b-navbar-item>
+          <b-navbar-item tag="router-link" :to="{ path: '2000' }">Années 2000</b-navbar-item>
+          <b-navbar-item tag="router-link" :to="{ path: '2010' }">Années 2010</b-navbar-item>
+        </b-navbar-dropdown>
+        <b-navbar-item href="#">Statistiques</b-navbar-item>
+      </template>
+
+      <template slot="end">
+        <b-navbar-item href="#">Admin</b-navbar-item>
+      </template>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Year2000.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <b-table :data='movies' :columns='columns'></b-table>
+  </div>
+</template>
+
+<script>
+import movies from './../api/movies.js'
+
+export default {
+  name: 'Year2000',
+  data () {
+    return {
+      movies: movies.getMovies,
+      columns: [
+        {
+          field: 'rank',
+          label: '#',
+          numeric: true
+        },
+        {
+          field: 'title',
+          label: 'Titre'
+        },
+        {
+          field: 'episode',
+          label: 'Épisode'
+        }
+      ]
+    }
+  },
+  methods: {
+    getMovies () {
+      this.movies = movies._movies
+    }
+  },
+  created: function () {
+    this.getMovies()
+  }
+}
+</script>
+
+<style scoped>
+</style>
